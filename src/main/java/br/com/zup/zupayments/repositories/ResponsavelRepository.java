4b07ac198d6b38a419409f6c6176a33656c88a60
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-03 17:25:35 -0300

    Criar ResponsavelRepository

---
+++ src/main/java/br/com/zup/zupayments/repositories/ResponsavelRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.zupayments.repositories;
+
+import br.com.zup.zupayments.models.Responsavel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResponsavelRepository extends CrudRepository<Responsavel,String> {
+}
