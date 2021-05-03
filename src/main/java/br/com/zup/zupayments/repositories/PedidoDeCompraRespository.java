300b4132c8babd7386edd480e13e21d989da8da1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-03 16:37:53 -0300

    criar repositório de pedido de compra

---
+++ src/main/java/br/com/zup/zupayments/repositories/PedidoDeCompraRespository.java
@@ -0,0 +1,7 @@
+package br.com.zup.zupayments.repositories;
+
+import br.com.zup.zupayments.models.PedidoDeCompra;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PedidoDeCompraRespository extends CrudRepository<PedidoDeCompra, Long> {
+}
