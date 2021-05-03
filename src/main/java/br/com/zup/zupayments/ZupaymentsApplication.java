8cbe2c02262825669711784fa096f27dc0294898
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-03 15:48:06 -0300

    Configurar projeto inicial com banco de dados mysql

---
+++ src/main/java/br/com/zup/zupayments/ZupaymentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zupayments/ZupaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
