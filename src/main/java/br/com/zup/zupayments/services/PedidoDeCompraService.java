aa8a53794343925c9ae898d26f04fd1b725369f3
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-03 16:49:07 -0300

    Criar método para cadastrar um novo pedido de compra no serviço

--- src/main/java/br/com/zup/zupayments/services/PedidoDeCompraService.java
+++ src/main/java/br/com/zup/zupayments/services/PedidoDeCompraService.java
@@ -1,5 +1,8 @@
 package br.com.zup.zupayments.services;
 
+import br.com.zup.zupayments.models.Fornecedor;
+import br.com.zup.zupayments.models.PedidoDeCompra;
+import br.com.zup.zupayments.models.Responsavel;
 import br.com.zup.zupayments.repositories.PedidoDeCompraRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,6 +10,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class PedidoDeCompraService {
 
-    @Autowired
     private PedidoDeCompraRespository pedidoDeCompraRespository;
+    private final ResponsavelService responsavelService;
+    private final FornecedorService fornecedorService;
+
+    @Autowired
+    public PedidoDeCompraService(PedidoDeCompraRespository pedidoDeCompraRespository,
+                                 ResponsavelService responsavelService,
+                                 FornecedorService fornecedorService) {
+        this.pedidoDeCompraRespository = pedidoDeCompraRespository;
+        this.responsavelService = responsavelService;
+        this.fornecedorService = fornecedorService;
+    }
+
+    public PedidoDeCompra cadastrarNovoPedidoDeCompra(PedidoDeCompra pedidoDeCompra) {
+        pedidoDeCompra.setResponsavel(responsavelService.procurarResponsavelPorEmail(
+                pedidoDeCompra.getResponsavel().getEmail()));
+        pedidoDeCompra.setFornecedor(fornecedorService.procurarFornecedorPorCnpjOuCpf(
+                pedidoDeCompra.getFornecedor().getCnpjOuCpf()));
+        return pedidoDeCompraRespository.save(pedidoDeCompra);
+    }
 }
