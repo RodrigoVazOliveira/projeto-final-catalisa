fc124b42366b3335d0f596138c5a0584252e9386
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-04 16:16:01 -0300

    Criar classe NotaFiscalServiceTest

---
+++ src/test/java/br/com/zup/zupayments/services/NotaFiscalServiceTest.java
@@ -0,0 +1,10 @@
+package br.com.zup.zupayments.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotaFiscalServiceTest {
+
+
+}
