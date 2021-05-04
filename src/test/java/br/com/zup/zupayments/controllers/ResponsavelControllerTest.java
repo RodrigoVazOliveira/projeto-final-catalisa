206aebcc60d1edde6993e93024b9b56f1d55cc29
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-04 16:47:55 -0300

    Criar método para cadastro de sucesso!

--- src/test/java/br/com/zup/zupayments/controllers/ResponsavelControllerTest.java
+++ src/test/java/br/com/zup/zupayments/controllers/ResponsavelControllerTest.java
@@ -3,11 +3,17 @@ package br.com.zup.zupayments.controllers;
 import br.com.zup.zupayments.dtos.responsavel.entrada.CadastrarResponsavelDTO;
 import br.com.zup.zupayments.models.Responsavel;
 import br.com.zup.zupayments.services.ResponsavelService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(ResponsavelController.class)
 public class ResponsavelControllerTest {
@@ -36,5 +42,22 @@ public class ResponsavelControllerTest {
                 "FACILITIES",
                 true
         );
+    }
+
+    @Test
+    public void testarCadastrarResponsavelOk() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String entradaJson = objectMapper.writeValueAsString(this.cadastrarResponsavelDTO);
+        String saidaJson   = objectMapper.writeValueAsString(this.responsavel);
+
+        Mockito.when(responsavelService.cadastrarResponsavel(Mockito.any())).thenReturn(responsavel);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/responsaveis/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(entradaJson)
+        ).andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json(saidaJson));
     }
 }
