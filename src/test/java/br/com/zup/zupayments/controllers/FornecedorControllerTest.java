dfd983e1756cc8b26fbb7d4685505a6fc54be9db
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-04 13:56:35 -0300

    Criar setup de teste d econtroller

--- src/test/java/br/com/zup/zupayments/controllers/FornecedorControllerTest.java
+++ src/test/java/br/com/zup/zupayments/controllers/FornecedorControllerTest.java
@@ -1,6 +1,8 @@
 package br.com.zup.zupayments.controllers;
 
+import br.com.zup.zupayments.models.Fornecedor;
 import br.com.zup.zupayments.services.FornecedorService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,4 +16,23 @@ public class FornecedorControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    private Fornecedor fornecedor;
+
+    @BeforeEach
+    public void setup() {
+        this.fornecedor = new Fornecedor(
+                "23.524.377/0001-45",
+                "Empresa 1",
+                "Rua Dos bobos",
+                0,
+                "Sem teto",
+                "Sem Fim",
+                "Sem Estado",
+                "2434424232",
+                "(99) 9999-9999",
+                "rsdfasfdsdf@sfsd.com",
+                null
+        );
+    }
 }
