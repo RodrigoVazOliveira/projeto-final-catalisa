6c93cede330720d9a0d9fc95bf2209c014422ff4
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-04 18:07:33 -0300

    Criar objetos de NF,Fornecedor e Responsavel para teste

--- src/test/java/br/com/zup/zupayments/services/NotaFiscalServiceTest.java
+++ src/test/java/br/com/zup/zupayments/services/NotaFiscalServiceTest.java
@@ -1,10 +1,55 @@
 package br.com.zup.zupayments.services;
 
+import br.com.zup.zupayments.models.Fornecedor;
+import br.com.zup.zupayments.models.NotaFiscal;
+import br.com.zup.zupayments.models.Responsavel;
+import br.com.zup.zupayments.repositories.NotaFiscalRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 public class NotaFiscalServiceTest {
 
+    @Autowired
+    private NotaFiscalService notaFiscalService;
 
+    @MockBean
+    private NotaFiscalRepository notaFiscalRepository;
+    private NotaFiscal notaFiscalteste;
+
+    @BeforeEach
+        public void setup(){
+
+        this.notaFiscalteste = new NotaFiscal();
+
+        Responsavel responsavel = new Responsavel();
+        responsavel.setAtivo(true);
+        responsavel.setEmail("bomdia@zup.com.br");
+        responsavel.setNomeCompleto("bom dia silva");
+
+        Fornecedor fornecedor = new Fornecedor();
+        fornecedor.setRazaoSocial("Uma Empresa Ltda");
+        fornecedor.setAtivo(true);
+        fornecedor.setCategoriaDeCusto(null);
+        fornecedor.setEmail("ceo@umaempresa.com");
+        fornecedor.setCnpjOuCpf("12.234.543/8765-56");
+        fornecedor.setLogradouro("Rua da empresa");
+        fornecedor.setNumero(272);
+        fornecedor.setCep("12122976");
+        fornecedor.setBairro("Bairro da empresa");
+        fornecedor.setEstado("Estado da empresa");
+
+        this.notaFiscalteste.setId(1l);
+                this.notaFiscalteste.setNumeroDaNota(220904l);
+                this.notaFiscalteste.setFornecedor(fornecedor);
+                this.notaFiscalteste.setValorAPagar(300.50);
+                this.notaFiscalteste.setDataDeEmissao(LocalDate.now());
+                this.notaFiscalteste.setNumeroDaNota(4L);
+                this.notaFiscalteste.setDataDeEnvio(LocalDate.now());
+                this.notaFiscalteste.setResponsavel(responsavel);
+    }
 }
