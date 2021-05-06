28bda6f8b44419413f9658e61dbd26dcbaee6807
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-06 16:38:03 -0300

    Corrigir classe de excecao

--- src/main/java/br/com/zup/zupayments/exceptions/erros/ResponsavelJaCadastradoException.java
+++ src/main/java/br/com/zup/zupayments/exceptions/erros/ResponsavelJaCadastradoException.java
@@ -1,11 +1,35 @@
 package br.com.zup.zupayments.exceptions.erros;
 
-import br.com.zup.zupayments.exceptions.ExcecaoBasica;
-
 public class ResponsavelJaCadastradoException extends RuntimeException {
-
     private Integer status = 422;
     private String tipoDeErro = "Responsável já cadastrado";
     private String motivo = "Unprocesable Entity";
+
+    public ResponsavelJaCadastradoException(String message) {
+        super(message);
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getTipoDeErro() {
+        return tipoDeErro;
+    }
+
+    public void setTipoDeErro(String tipoDeErro) {
+        this.tipoDeErro = tipoDeErro;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
     }
 }
