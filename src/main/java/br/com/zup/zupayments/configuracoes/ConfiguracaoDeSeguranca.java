26ff629769a9d4f708bc795a6be7e1860a796402
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-07 15:29:11 -0300

    Refatorar acessos por perfil

--- src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
@@ -1,5 +1,6 @@
 package br.com.zup.zupayments.configuracoes;
 
+import br.com.zup.zupayments.enums.RolesEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -42,23 +43,23 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
 
         http.authorizeRequests()
-                .antMatchers(HttpMethod.PATCH,PUBLIC_MATCHES_PATCH).hasRole("PERFIL_MASTER")
-                .antMatchers(HttpMethod.POST,PUBLIC_MATCHES_POST).hasRole("PERFIL_MASTER")
-                .antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).hasRole("PERFIL_MASTER")
-                .antMatchers(HttpMethod.GET,"/fornecedores/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.POST,"/fornecedores/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.PATCH,"/fornecedores/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.GET,"/notas_ficais/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.GET,"/pedidos/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.POST,"/pedidos/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.PATCH,"/pedidos/").hasRole("PERFIL_COMPRAS")
-                .antMatchers(HttpMethod.GET,"/notas_ficais/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.POST,"/notas_ficais/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.PATCH,"/notas_ficais/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.GET,"/responsaveis/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.POST,"/responsaveis/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.PATCH,"/responsaveis/").hasRole("PERFIL_FINANCEIRO")
-                .antMatchers(HttpMethod.GET,"/pedidos/").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.PATCH,PUBLIC_MATCHES_PATCH).hasRole(String.valueOf(RolesEnum.PERFIL_MASTER))
+                .antMatchers(HttpMethod.POST,PUBLIC_MATCHES_POST).hasRole(String.valueOf(RolesEnum.PERFIL_MASTER))
+                .antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).hasRole(String.valueOf(RolesEnum.PERFIL_MASTER))
+                .antMatchers(HttpMethod.GET,"/fornecedores/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.POST,"/fornecedores/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.PATCH,"/fornecedores/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.GET,"/notas_ficais/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.GET,"/pedidos/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.POST,"/pedidos/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.PATCH,"/pedidos/").hasRole(String.valueOf(RolesEnum.PERFIL_COMPRAS))
+                .antMatchers(HttpMethod.GET,"/notas_ficais/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.POST,"/notas_ficais/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.PATCH,"/notas_ficais/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.GET,"/responsaveis/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.POST,"/responsaveis/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.PATCH,"/responsaveis/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
+                .antMatchers(HttpMethod.GET,"/pedidos/").hasRole(String.valueOf(RolesEnum.PERFIL_FINANCEIRO))
 
                 .anyRequest().authenticated();
     }
