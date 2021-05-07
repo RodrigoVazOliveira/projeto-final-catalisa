9d355811536818c19763eb925b8042845defe06f
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-07 13:47:53 -0300

    Autorizar acessos conforme cada perfil

--- src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
@@ -16,31 +16,50 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHES_GET = {
             "/fornecedores/**",
-            "/notas_ficais/**",
             "/pedidos/**",
-            "/responsaveis/**"
+            "/responsaveis/**",
+            "/notas_fiscais/**"
     };
+
     private static final String[] PUBLIC_MATCHES_POST = {
             "/fornecedores/**",
-            "/notas_ficais/**",
             "/pedidos/**",
-            "/responsaveis/**"
+            "/responsaveis/**",
+            "/notas_fiscais/**"
     };
     private static final String[] PUBLIC_MATCHES_PATCH = {
             "/fornecedores/**",
-            "/notas_ficais/**",
             "/pedidos/**",
-            "/responsaveis/**"
+            "/responsaveis/**",
+            "/notas_fiscais/**"
     };
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable();
         http.cors();
 
-        http.authorizeRequests().antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).permitAll()
-                .antMatchers(HttpMethod.POST,PUBLIC_MATCHES_POST).permitAll()
-                .antMatchers(HttpMethod.PATCH,PUBLIC_MATCHES_PATCH).permitAll()
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.PATCH,PUBLIC_MATCHES_PATCH).hasRole("PERFIL_MASTER")
+                .antMatchers(HttpMethod.POST,PUBLIC_MATCHES_POST).hasRole("PERFIL_MASTER")
+                .antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).hasRole("PERFIL_MASTER")
+                .antMatchers(HttpMethod.GET,"/fornecedores").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.POST,"/fornecedores").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.PATCH,"/fornecedores").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.GET,"/notas_ficais").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.GET,"/pedidos").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.POST,"/pedidos").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.PATCH,"/pedidos").hasRole("PERFIL_COMPRAS")
+                .antMatchers(HttpMethod.GET,"/notas_ficais").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.POST,"/notas_ficais").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.PATCH,"/notas_ficais").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.GET,"/responsaveis").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.POST,"/responsaveis").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.PATCH,"/responsaveis").hasRole("PERFIL_FINANCEIRO")
+                .antMatchers(HttpMethod.GET,"/pedidos").hasRole("PERFIL_FINANCEIRO")
+
                 .anyRequest().authenticated();
     }
     @Bean
