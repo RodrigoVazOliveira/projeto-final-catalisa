c99521f89aa8a1be1cf936093b547d9b28112c80
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-07 14:15:39 -0300

    Refatorar endpoints

--- src/main/java/br/com/zup/zupayments/enums/RolesEnum.java
+++ src/main/java/br/com/zup/zupayments/enums/RolesEnum.java
@@ -1,21 +1,20 @@
 package br.com.zup.zupayments.enums;
 
 public enum RolesEnum {
-    PERFIL_MASTER(2,"ROLE_MASTER","hasRole('PERFIL_MASTER')","/notas_ficais/**","Administra tudo"),
-    PERFIL_FINANCEIRO(1,"ROLE_FINANCEIRO","hasRole('PERFIL_FINANCEIRO')","/notas_ficais/**","Administra notas fiscais"),
-    PERFIL_COMPRAS(3,"ROLE_COMPRAS","hasRole('PERFIL_COMPRAS')","/fornecedores/**","Administra fornecedores");
+
+    PERFIL_FINANCEIRO(2,"ROLE_FINANCEIRO","hasRole('PERFIL_FINANCEIRO')","Administra notas fiscais e responsáveis"),
+    PERFIL_MASTER(1,"ROLE_MASTER","hasRole('PERFIL_MASTER')","Administra tudo"),
+    PERFIL_COMPRAS(3,"ROLE_COMPRAS","hasRole('PERFIL_COMPRAS')","Administra fornecedores e pedidos");
 
     private Integer id;
     private String role;
     private String roleAcess;
-    private String url;
     private String descricao;
 
-    RolesEnum(Integer id, String role, String roleAcess, String url, String descricao) {
+    RolesEnum(Integer id, String role, String roleAcess, String descricao) {
         this.id = id;
         this.role = role;
         this.roleAcess = roleAcess;
-        this.url = url;
         this.descricao = descricao;
     }
 
@@ -43,13 +42,6 @@ public enum RolesEnum {
         this.roleAcess = roleAcess;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public String getDescricao() {
         return descricao;
