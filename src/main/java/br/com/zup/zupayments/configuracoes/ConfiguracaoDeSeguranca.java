262b92a14bb9d20bad72beaaa4e7c2d133f1548b
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-05-07 11:35:45 -0300

    Definir configurações gerais de segurança

--- src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/zupayments/configuracoes/ConfiguracaoDeSeguranca.java
@@ -1,4 +1,52 @@
 package br.com.zup.zupayments.configuracoes;
 
-public class ConfiguracaoDeSeguranca {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHES_GET = {
+            "/fornecedores/**",
+            "/notas_ficais/**",
+            "/pedidos/**",
+            "/responsaveis/**"
+    };
+    private static final String[] PUBLIC_MATCHES_POST = {
+            "/fornecedores/**",
+            "/notas_ficais/**",
+            "/pedidos/**",
+            "/responsaveis/**"
+    };
+    private static final String[] PUBLIC_MATCHES_PATCH = {
+            "/fornecedores/**",
+            "/notas_ficais/**",
+            "/pedidos/**",
+            "/responsaveis/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.cors();
+
+        http.authorizeRequests().antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).permitAll()
+                .antMatchers(HttpMethod.POST,PUBLIC_MATCHES_POST).permitAll()
+                .antMatchers(HttpMethod.PATCH,PUBLIC_MATCHES_PATCH).permitAll()
+                .anyRequest().authenticated();
+    }
+    @Bean
+    CorsConfigurationSource configuracaoDeCors(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
