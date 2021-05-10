a61e5981113573ac33619aaf55fcaff1a5734e10
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-10 13:49:10 -0300

    Criar método para cadastrar novo usuário

--- src/main/java/br/com/zup/zupayments/services/UsuarioService.java
+++ src/main/java/br/com/zup/zupayments/services/UsuarioService.java
@@ -1,7 +1,9 @@
 package br.com.zup.zupayments.services;
 
+import br.com.zup.zupayments.models.Usuario;
 import br.com.zup.zupayments.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,13 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    public Usuario cadastrarNovoUsuario(Usuario usuario) {
+        usuario.setSenha(bCryptPasswordEncoder.encode(usuario.getSenha()));
+        return usuarioRepository.save(usuario);
+    }
+
+    
 }
