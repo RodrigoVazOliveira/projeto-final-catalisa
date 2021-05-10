86197f2b568e457bc5084440b43be3f74abc225c
Kaio <kaio.moraes@zup.com.br>
2021-05-10 16:15:03 -0300

    Criar classe FiltroDeAutorizacao.

--- src/main/java/br/com/zup/zupayments/jwt/UserDetailsService.java
+++ src/main/java/br/com/zup/zupayments/jwt/UserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class UsuarioLoginService implements UserDetailsService {
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
