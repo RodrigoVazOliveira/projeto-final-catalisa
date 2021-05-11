8655c57597e20f17d6eddc628234c0e54f2f9e4d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-11 08:11:32 -0300

    Documentando API

--- src/main/java/br/com/zup/zupayments/controllers/NotaFiscalController.java
+++ src/main/java/br/com/zup/zupayments/controllers/NotaFiscalController.java
@@ -3,12 +3,15 @@ package br.com.zup.zupayments.controllers;
 import br.com.zup.zupayments.dtos.notafiscal.entrada.CadastrarNotaFiscalDTO;
 import br.com.zup.zupayments.models.NotaFiscal;
 import br.com.zup.zupayments.services.NotaFiscalService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("notas_fiscais/")
+@Api(value = "API REST de Notas fiscais")
 public class NotaFiscalController {
 
     @Autowired
@@ -16,12 +19,14 @@ public class NotaFiscalController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Cadastrar uma nova nota fiscal")
     public NotaFiscal cadastroNotaFiscal(@RequestBody CadastrarNotaFiscalDTO cadastrarNotaFiscalDTO){
         return notaFiscalService.cadastrarNotaFiscal(cadastrarNotaFiscalDTO.converterDtoParaModelo());
     }
 
     @PatchMapping("{id}/")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Cancelar uma nota fiscal pelo id")
     public NotaFiscal cancelamentoDeNotaFiscal(@PathVariable Long id){
         NotaFiscal objNotaFiscal = notaFiscalService.cancelarNF(id);
         return objNotaFiscal;
--- src/main/java/br/com/zup/zupayments/controllers/UsuarioController.java
+++ src/main/java/br/com/zup/zupayments/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import br.com.zup.zupayments.dtos.usuario.entrada.CadastrarUsuarioDTO;
 import br.com.zup.zupayments.dtos.usuario.entrada.NivelDeAcessoUsuarioDTO;
 import br.com.zup.zupayments.dtos.usuario.saida.UsuarioDTO;
 import br.com.zup.zupayments.services.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("usuarios/")
+@Api(value = "API REST de usuários")
 public class UsuarioController {
 
     @Autowired
@@ -19,24 +22,28 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Cadastrar um novo usuário")
     public void cadastrarNovoUsuario(@RequestBody @Valid CadastrarUsuarioDTO usuario) {
         usuarioService.cadastrarNovoUsuario(usuario.converterDtoParaModelo());
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Mostrar todos os usuários")
     public Iterable<UsuarioDTO> mostarTodosUsuarios() {
         return UsuarioDTO.converterListaDeModeloParaListaDto(usuarioService.obterTodosUsuarios());
     }
 
     @PatchMapping("ativo")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "Ativar ou desativar um usuário pelo id")
     public void ativarOuDesativarUsuario(@RequestParam(name = "idUsuario") Long id) {
         usuarioService.ativarOuDesativarUsuario(id);
     }
 
     @PatchMapping("nivel")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Alterar o nível de acesso do usuário")
     public UsuarioDTO atualizarNivelDeAcesso(@RequestParam(name = "idUsuario") Long id,
                                              @RequestBody NivelDeAcessoUsuarioDTO nivelDeAcessoUsuarioDTO) {
         return UsuarioDTO.converterModeloParaDTO(
